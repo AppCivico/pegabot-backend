615ca16e0f38abd9f6e3b8c1b9c25f7a75160a96
Valentin Guillot <valguillot@gmail.com>
2018-10-10 14:30:50 -0300

    fix

--- main.js
+++ main.js
@@ -54,7 +54,7 @@ app.get("/botometer", function(request, response) {
           response.json(err)
           return;
         }
-        result.profiles[0].language_dependent.content = 0;
+        result.profiles[0].language_dependent.content.value = 0;
         response.json(result)
       })
     }
