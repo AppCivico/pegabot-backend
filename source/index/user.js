d8a555215a083c7d56a7c18fe53eff7de24e8f75
Jordan Victor Scher <jordanvscher@hotmail.com>
2020-04-30 17:19:10 -0300

    update spottingbot user

--- source/index/user.js
+++ source/index/user.js
@@ -1,59 +1,83 @@
 const library = require('../library');
 
-module.exports = function (data) {
+module.exports = (data) => {
   return new Promise((resolve) => {
-    let friends_ratio = 0;
-    let name = data.name;
-    let name_length = data.name.length;
-    let screen_name = data.screen_name;
-    let screen_name_length = data.screen_name.length;
-    let screen_name_digits = library.getNumberOfDigit(data.screen_name);
-    let description_length = data.description.length;
-    let profile_image = data.profile_image_url;
-    let age = library.convertTwitterDateToDaysAge(data.created_at);
-    let ratio_tweets_day = data.statuses_count / age;
-    if (data.verified) {
-      resolve([0, 3]);
-    }
+    // if user is verified, the final result will be 0
+    if (data.verified) return resolve([0, 3]);
+
+    // let friends_ratio = 0;
+    const name = data.name;
+    const nameLength = data.name.length;
+    const screenName = data.screen_name;
+    const screenNameLength = screenName.length;
+    const screenNameDigits = library.getNumberOfDigit(screenName);
+    const descriptionLength = data.description.length;
+    const profileImage = data.profile_image_url;
+
+    // get tweets ratio
+    const age = library.convertTwitterDateToDaysAge(data.created_at);
+    const ratio_tweets_day = data.statuses_count / age;
+
+    // Similarity score
     let nameCut = name.replace(/[\s_]+/g, '');
-    let screenNameCut = screen_name.replace(/[\s_]+/g, '');
-    let nameSimilarityScore = 1 - library.similarity(nameCut, screenNameCut);
-    if (name.toLowerCase().indexOf('bot') !== -1 || screen_name.toLowerCase().indexOf('bot') !== -1) nameSimilarityScore = 1;
+    let screenNameCut = screenName.replace(/[\s_]+/g, '');
+    let nameSimilarityScore;
+
+    // searches for "bot" substring
+    if (name.toLowerCase().indexOf('bot') !== -1 || screenName.toLowerCase().indexOf('bot') !== -1) nameSimilarityScore = 1;
+    else nameSimilarityScore = 1 - library.similarity(nameCut, screenNameCut); // calculates the similarity
+
+    // Number of digits
     let numberDigitScore = 0.15;
-    if (screen_name_digits > 2) {
-      numberDigitScore = screen_name_digits * 0.12;
-      if (numberDigitScore > 1) numberDigitScore = 1;
+    if (screenNameDigits > 2) {
+      numberDigitScore = screenNameDigits * 0.12;
+      numberDigitScore = Math.min(numberDigitScore, 1);
     }
+
+    // Length of name
     let nameLengthScore = 0.15;
-    if (name_length > 15) {
-      nameLengthScore = name_length * 0.009;
-      if (nameLengthScore > 1) nameLengthScore = 1;
+    if (nameLength > 15) {
+      nameLengthScore = nameLength * 0.009;
+      nameLengthScore = Math.min(nameLengthScore, 1);
     }
+
+    // Length of screen name
     let screenNameLengthScore = 0.15;
-    if (screen_name_length > 10) {
-      screenNameLengthScore = screen_name_length * 0.012;
-      if (screenNameLengthScore > 1) screenNameLengthScore = 1;
+    if (screenNameLength > 10) {
+      screenNameLengthScore = screenNameLength * 0.012;
+      screenNameLengthScore = Math.min(screenNameLengthScore, 1);
     }
+
+    // Length of description
     let descriptionLengthScore = 0.15;
-    if (description_length < 10) {
-      descriptionLengthScore = description_length * 0.1;
-      descriptionLengthScore = 1 - descriptionLengthScore;
-      if (descriptionLengthScore < 0) descriptionLengthScore = 0;
+    if (descriptionLength < 10) {
+      descriptionLengthScore = 1 - (descriptionLength * 0.1);
+      descriptionLengthScore = Math.max(descriptionLengthScore, 0);
     }
+
+    // Age
     let ageScore = 1;
     if (age > 90) {
       ageScore = ageScore - age * 0.001;
-      if (ageScore < 0) ageScore = 0;
+      ageScore = Math.max(ageScore, 0);
     }
-    let imageScore;
-    if (profile_image === null) imageScore = 1;
-    else imageScore = 0.15;
+
+    // Image Score
+    let imageScore = 0.15;
+    if (!profileImage) imageScore = 1;
+
+    // Tweets by day
     let ratioTweetScore = ratio_tweets_day * 0.05;
+
+    // Favorite Score
     let favoritesScore = 1 - data.favourites_count * 0.01;
-    if (favoritesScore < 0) favoritesScore = 0;
-    let userScore = (nameSimilarityScore + numberDigitScore + nameLengthScore + screenNameLengthScore + descriptionLengthScore + ageScore + ratioTweetScore + favoritesScore + imageScore) / 9;
-    if (userScore < 0) userScore = 0;
-    else if (userScore > 1) userScore = 1;
-    resolve([userScore, 1]);
+    favoritesScore = Math.max(favoritesScore, 0);
+
+    const totalSum = nameSimilarityScore + numberDigitScore + nameLengthScore + screenNameLengthScore + descriptionLengthScore + ageScore + ratioTweetScore + favoritesScore + imageScore;
+    let userScore = totalSum / 9;
+
+    // limits userScore to range 0 and 1
+    userScore = Math.min(1, Math.max(0, userScore));
+    return resolve([userScore, 1]);
   });
 };
