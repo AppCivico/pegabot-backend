a18cfbd5d7c3f0f4287dac29a45cf7efbacf2dca
Jordan Victor Scher <jordanvscher@hotmail.com>
2020-04-30 20:13:17 -0300

    Update temporal

--- documentation/Temporal.md
+++ documentation/Temporal.md
@@ -1,14 +1,10 @@
-PDF To Markdown Converter
-Debug View
-Result View
-
 **Key Temporal Profile:**
 
 Data to collect:
 The date of all tweets posted in the timeline sample
 Age of the profile
 
-Preparation of data:
+Data Preparation:
 Put all date of posted tweet in an array
 
 List of subindex:
@@ -16,26 +12,23 @@ List of subindex:
 - Delay between two tweets
 - Ratio tweets by day
 
-Calcul of the probability:
+Probability Calculation:
 
-- Substract all date of a created tweet to the date of the tweet created previously. Put this diff in an
-array if it is not already present
-- Divide the number of total tweets by the number of day the profile was created and multiplicate it
-by x0,025 to not make this score impact a lot the final
+- Subtract date of a created tweet from the date of the previous tweet. 
+Put this diff in an array if it is not already present.
+- Divide the number of total tweets by the number of days since the profile was created and multiply it
+by 0.025 to reduces it's impact on the final score.
 
-Then to get the temporal score, we add 2 to the length of the diff array and divide the result by the
+Then, to get the temporal score, we add 2 to the length of the diff array and divide the result by the
 size of the sample. The result is removed from 1. Finally the tweets ratio score is added to get the
 final score.
 
-Score = (1 - (Size of the diff array + 2) / Number of data) + Ratio tweets by day * 0,025
+Score = (1 - (Size of the diff array + 2) / Number of dates) + Ratio tweets by day * 0.025
 
 More explanations:
-A bot can be scheduled to send tweet every X seconds, minute or hours. This index is here for get
-this.
+A bot can be scheduled to send a tweet every X seconds, minutes or hours. This index is here to account for that.
 In case of the tweets do not looks so scheduled, we also get the ratio of tweets by day since the
-profile creation, a number too high is suspicious
+profile creation, a number too high is suspicious.
 
 
 
-This is a offline tool, your data stays locally and is not send to any server!
-Feedback & Bug Reports
--- source/index/temporal.js
+++ source/index/temporal.js
@@ -1,35 +1,34 @@
 const library = require('../library');
 
-module.exports = function (data) {
-  return new Promise((resolve) => {
-    let creationArray = [];
-    let delayTwoTweets = [];
-    // Put the date of each tweets in array
-    data.forEach(function (current) {
-      let date = new Date(current.created_at + 'Z');
-      creationArray.push(date);
-    });
-    // Calculate the difference time between each tweet
-    for (let i = 0; i < creationArray.length - 1; i++) {
-      let diff = creationArray[i].getTime() - creationArray[i + 1].getTime();
-      // If the difference time is not already in the array, put it
-      if (delayTwoTweets.indexOf(diff) === -1) {
-        delayTwoTweets.push(diff);
-        // The shorter this array is, the more tweets are posted in a very regular way
-      }
-    }
-    let age = library.convertTwitterDateToDaysAge(data[0].user.created_at);
-    let number_of_tweets = data[0].user.statuses_count;
-    let ratio_tweets_day = number_of_tweets / age;
-    let ratioTweetScore = ratio_tweets_day * 0.015;
-    let temporal_score = ((1 - (delayTwoTweets.length + 2) / creationArray.length) + ratioTweetScore);
-    if (temporal_score < 0) {
-      temporal_score = 0;
-    }
-    let weight = 1;
-    if (temporal_score === 0) {
-      weight += 1;
-    }
-    resolve([temporal_score, weight]);
+module.exports = (data) => new Promise((resolve) => {
+  const creationArray = [];
+  const delayTwoTweets = [];
+
+  // Put the date of each tweet in array
+  data.forEach((current) => {
+    const date = new Date(`${current.created_at}Z`);
+    creationArray.push(date);
   });
-};
+
+  // Calculate the difference in time between each tweet
+  for (let i = 0; i < creationArray.length - 1; i++) { // eslint-disable-line no-plusplus
+    const diff = creationArray[i].getTime() - creationArray[i + 1].getTime();
+
+    // If the difference time is not already in the array, add it in
+    if (delayTwoTweets.indexOf(diff) === -1) delayTwoTweets.push(diff);
+    // The shorter this array is, the more tweets are posted in a suspiciously regular frequency
+  }
+
+  const age = library.convertTwitterDateToDaysAge(data[0].user.created_at);
+  const numberOfTweets = data[0].user.statuses_count;
+  const ratioTweetsDay = numberOfTweets / age;
+  const ratioTweetScore = ratioTweetsDay * 0.015;
+
+  let temporalScore = ((1 - (delayTwoTweets.length + 2) / creationArray.length) + ratioTweetScore);
+  if (temporalScore < 0) temporalScore = 0;
+
+  let weight = 1;
+  if (temporalScore === 0) weight += 1;
+
+  resolve([temporalScore, weight]);
+});
