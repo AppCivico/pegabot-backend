177e7161d513f2bca2723ad326f16126bd4f930d
Jordan Victor Scher <jordanvscher@hotmail.com>
2020-04-30 19:52:21 -0300

    update friends

--- documentation/Friend.md
+++ documentation/Friend.md
@@ -7,11 +7,11 @@ Number of friends
 Date of creation
 Number of Tweets
 
-Preparation of data:
+Data Preparation:
 For each data collected, put it in a distribution array that contains an unique element of each
 different element collected.
 For example, if we have five times the data “UTC Offset” equal to UTC -03, we put this data only
-once in the array
+once in the array.
 
 List of subindex:
 
@@ -21,27 +21,27 @@ List of subindex:
 - Followers
 - Tweets number
 
-Calcul of the probability:
+Probability calculation:
 
 - Length of the offset array divided by 8
 - Length of the age array divided by 50
 - Length of the friends array divided by 50
-- Length of the followers array divided by 50
+- Length of the followers array divided by 75
 - Length of the tweets number array divided by 50
-- If any of the score above is more than 1, we fix it to 1.
+- If any of the score above bigger than 1, we fix it to 1.
 
 Then, for the score of everything above, we multiply by two the offset score and add it to all others
 score. Then, everything is divided by 10.
 
 All the calculation above are made once for the friends of the profile analyzed, and once for the
 followers of the profile.
-Once we got the two scores. We calculate the final friend index score by multiplicated the score of
-followers by 1,5x and add it to the friends score. Finally, we divide it by 3.
+Once we got the two scores. We calculate the final friend index score by multiplying the score of
+followers by 1.5x and add it to the friends score. Finally, we divide it by 3.
 
 More explanations:
-The offset score is the most important, this is the reason why it count twice in the final score.
-This is not very common for an user to have friends/followers from a lot of different timezone.
-We consider others data like more common to be different and care less about their value in the
+The offset score is the most important, this is the reason why it's counted twice in the final score.
+It isn't very common for an user to have friends/followers from a lot of different timezones.
+We consider others types of data to usually be different and care less about their values in the
 final score.
-The score of friends is most important than the score of followers since friends are chosen by the
-user, so this score count 1,5x more in the final score.
+The score of friends is more important than the score of followers since friends are chosen by the
+user, so this score count 1.5x more in the final score.
--- source/index/friends.js
+++ source/index/friends.js
@@ -1,47 +1,57 @@
 const library = require('../library');
 
-module.exports = function (data) {
-  return new Promise((resolve) => {
-    let distribution_friends_offset = [];
-    let distribution_friends_age = [];
-    let distribution_friends_friends = [];
-    let distribution_friends_followers = [];
-    let distribution_friends_tweets = [];
-    let friends_score = 0;
-    data.users.forEach(function (current) {
-      // Add news values for each distribution array if the value is not present yet
-      if (current.utc_offset != null && distribution_friends_offset.indexOf(current.utc_offset) === -1) {
-        distribution_friends_offset.push(current.utc_offset);
-      }
-      let current_age = Math.round(library.convertTwitterDateToDaysAge(current.created_at) / 100) * 100;
-      if (current.created_at != null && distribution_friends_age.indexOf(current_age) === -1) {
-        distribution_friends_age.push(current_age);
-      }
-      let current_friends = Math.round(current.friends_count / 100) * 100;
-      if (current.friends_count != null && distribution_friends_friends.indexOf(current_friends) === -1) {
-        distribution_friends_friends.push(current_friends);
-      }
-      let current_followers = Math.round(current.followers_count / 100) * 100;
-      if (current.followers_count != null && distribution_friends_followers.indexOf(current_followers) === -1) {
-        distribution_friends_followers.push(current_followers);
-      }
-      let current_statuses = Math.round(current.statuses_count / 100) * 100;
-      if (current.statuses_count != null && distribution_friends_tweets.indexOf(current_statuses) === -1) {
-        distribution_friends_tweets.push(current_statuses);
-      }
-    });
-    let score_offset = distribution_friends_offset.length / 8;
-    let score_age = distribution_friends_age.length / 50;
-    let score_friends = distribution_friends_friends.length / 50;
-    let score_followers = distribution_friends_followers.length / 75;
-    let score_statuses = distribution_friends_tweets.length / 50;
-    if (score_offset > 1) score_offset = 1;
-    if (score_age > 1) score_age = 1;
-    if (score_friends > 1) score_friends = 1;
-    if (score_followers > 1) score_followers = 1;
-    if (score_statuses > 1) score_statuses = 1;
-    friends_score = ((score_offset * 2) + score_age + score_friends + score_followers + score_statuses) / (5 * 2);
-    let weight = 1;
-    resolve([friends_score, weight]);
+module.exports = (data) => new Promise((resolve) => {
+  let friendsScore = 0;
+  const distributionFriendsOffset = [];
+  const distributionFriendsAge = [];
+  const distributionFriendsFriends = [];
+  const distributionFriendsFollowers = [];
+  const distributionFriendsTweets = [];
+
+  data.users.forEach((current) => {
+    // Add news values for each distribution array if the value is not present yet
+    if (current.utc_offset !== null && distributionFriendsOffset.indexOf(current.utc_offset) === -1) {
+      distributionFriendsOffset.push(current.utc_offset);
+    }
+
+    const currentAge = Math.round(library.convertTwitterDateToDaysAge(current.created_at) / 100) * 100;
+    if (current.created_at !== null && distributionFriendsAge.indexOf(currentAge) === -1) {
+      distributionFriendsAge.push(currentAge);
+    }
+
+    const currentFriends = Math.round(current.friends_count / 100) * 100;
+    if (current.friends_count !== null && distributionFriendsFriends.indexOf(currentFriends) === -1) {
+      distributionFriendsFriends.push(currentFriends);
+    }
+
+    const currentFollowers = Math.round(current.followers_count / 100) * 100;
+    if (current.followers_count !== null && distributionFriendsFollowers.indexOf(currentFollowers) === -1) {
+      distributionFriendsFollowers.push(currentFollowers);
+    }
+
+    const currentStatuses = Math.round(current.statuses_count / 100) * 100;
+    if (current.statuses_count !== null && distributionFriendsTweets.indexOf(currentStatuses) === -1) {
+      distributionFriendsTweets.push(currentStatuses);
+    }
   });
-};
+
+  let scoreOffset = distributionFriendsOffset.length / 8;
+  scoreOffset = Math.min(scoreOffset, 1);
+
+  let scoreAge = distributionFriendsAge.length / 50;
+  scoreAge = Math.min(scoreAge, 1);
+
+  let scoreFriends = distributionFriendsFriends.length / 50;
+  scoreFriends = Math.min(scoreFriends, 1);
+
+  let scoreFollowers = distributionFriendsFollowers.length / 75;
+  scoreFollowers = Math.min(scoreFollowers, 1);
+
+  let scoreStatuses = distributionFriendsTweets.length / 50;
+  scoreStatuses = Math.min(scoreStatuses, 1);
+
+  friendsScore = ((scoreOffset * 2) + scoreAge + scoreFriends + scoreFollowers + scoreStatuses) / (5 * 2);
+  const weight = 1;
+
+  resolve([friendsScore, weight]);
+});
