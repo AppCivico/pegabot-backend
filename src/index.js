8c908ee127d339503565f4075472f369587bfe57
Junior Moraes <juniorfvox@gmail.com>
2020-05-07 19:35:27 -0300

    refactor: avoid eslint warnings on index file

--- src/index.js
+++ src/index.js
@@ -24,78 +24,139 @@ const config = {
   consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
 };
 
-const cache_duration = process.env.CACHE_DURATION || 2592000;
+const cacheDuration = process.env.CACHE_DURATION || 2592000;
 
-app.get('/botometer', function (request, response) {
-  let target = request.query.search_for;
-  let profile = request.query.profile;
-  let limit = request.query.limit;
-  let authenticated = request.query.authenticated;
-  let key = target + ':' + profile;
-  let cachedKey = mcache.get(key);
+const requestTwitterList = (client, searchFor, profile, limit, callback) => {
+  let cursor = -1;
+  const list = [];
+  let total = 0;
+  const param = {
+    screen_name: profile,
+  };
+
+  client.get('users/show', param, (error, tweets, responseTwitterUser) => {
+    if (error) {
+      console.log(error);
+    }
+    total = JSON.parse(responseTwitterUser.body)[`${searchFor}_count`];
+
+    async.whilst(
+      () => cursor === -1,
+      (next) => {
+        const params = {
+          screen_name: profile,
+          count: limit,
+          cursor,
+        };
+
+        client.get(`${searchFor}/list`, params, (err, tweets, responseTwitter) => {
+          if (err) {
+            console.log(err);
+            next(err);
+          } else {
+            const data = JSON.parse(responseTwitter.body);
+            data.users.forEach((current) => {
+              list.push(current);
+            });
+            cursor = data.next_cursor;
+            next();
+          }
+        });
+      },
+      (err) => {
+        const object = {
+          metadata: {
+            count: list.length,
+            total,
+          },
+          profiles: [],
+        };
+        list.forEach((value) => {
+          object.profiles.push({
+            username: value.screen_name,
+            url: `https://twitter.com/${value.screen_name}`,
+            avatar: value.profile_image_url,
+            user_profile_language: value.lang,
+          });
+        });
+        if (err) {
+          object.metadata.error = err;
+        }
+        callback(object);
+      },
+    );
+  });
+}
+
+app.get('/botometer', (request, response) => {
+  const target = request.query.search_for;
+  const { profile } = request.query;
+  let { limit } = request.query;
+  const { authenticated } = request.query;
+  const key = `${target}:${profile}`;
+  const cachedKey = mcache.get(key);
+
   if (!limit || limit > 200) {
     limit = 200;
   }
-  console.log('Request ' + target + ' for ' + profile);
+  console.log(`Request ${target} for ${profile}`);
   if (typeof target === 'undefined' || typeof profile === 'undefined') {
     response.status(400).send('One parameter is missing');
   } else if (cachedKey) {
     response.send(cachedKey);
   } else if (target === 'profile') {
-    spottingbot(profile, config, {friend: false, sentiment: false}, function (err, result) {
+    spottingbot(profile, config, { friend: false, sentiment: false }, (err, result) => {
       if (err) {
-        let err = {
+        return response.json({
           metadata: {
-            error: profile + ' não encontrado',
+            error: `${profile} não encontrado`,
           },
-        };
-        response.json(err);
-        return;
+        });
       }
       result.profiles[0].language_dependent = null;
-      result.profiles.forEach((profile) => {
-        profile.bot_probability.all = Math.min(profile.bot_probability.all, 0.99);
-      });
+      // result.profiles.forEach((profile) => {
+      //   profile.bot_probability.all = Math.min(profile.bot_probability.all, 0.99);
+      // });
       response.json(result);
     });
   } else if (target === 'followers' || target === 'friends') {
     if (authenticated === 'true') {
       let token = request.query.oauth_token;
-      let token_secret = mcache.get(token);
-      let verifier = request.query.oauth_verifier;
-      let oauth = {
+      let tokenSecret = mcache.get(token);
+      const verifier = request.query.oauth_verifier;
+      const oauth = {
         consumer_key: config.consumer_key,
         consumer_secret: config.consumer_secret,
-        token: token,
-        token_secret: token_secret,
-        verifier: verifier,
+        token,
+        tokenSecret,
+        verifier,
       };
-      let url = 'https://api.twitter.com/oauth/access_token';
-      let request2 = require('request');
-      request2.post({url: url, oauth: oauth}, function (e, r, body) {
-        var perm_data = qs.parse(body);
-        token = perm_data.oauth_token;
-        token_secret = perm_data.oauth_token_secret;
-        let client = new Twitter({
+      const url = 'https://api.twitter.com/oauth/access_token';
+      const request2 = require('request');
+      request2.post({ url, oauth }, (e, r, body) => {
+        const permData = qs.parse(body);
+        token = permData.oauth_token;
+        tokenSecret = permData.oauth_token_secret;
+        const client = new Twitter({
           consumer_key: config.consumer_key,
           consumer_secret: config.consumer_secret,
           access_token_key: token,
-          access_token_secret: token_secret,
+          access_token_secret: tokenSecret,
         });
-        requestTwitterList(client, target, profile, limit, function (object) {
+        requestTwitterList(client, target, profile, limit, (object) => {
           if (typeof object.metadata.error === 'undefined') {
-            mcache.put(key, JSON.stringify(object), cache_duration * 1000);
+            mcache.put(key, JSON.stringify(object), cacheDuration * 1000);
           }
           response.json(object);
         });
       });
     } else {
-      getTokenUrl(request, target, profile, limit, function (err, uri) {
+      getTokenUrl(request, target, profile, limit, (err, uri) => {
         if (err) {
           response.status(500).send(err);
           return;
         }
-        let object = {
+        const object = {
           request_url: uri,
         };
         response.json(object);
@@ -111,88 +172,30 @@ function getTokenUrl(req, search_for, profile, limit, callback) {
   if (req.connection.encrypted) {
     ssl = 'https://';
   }
-  let oauth = {
-    callback: ssl + req.headers.host + '/resultados?socialnetwork=twitter&authenticated=true&profile=' + profile + '&search_for=' + search_for + '&limit=' + limit + '#conteudo',
+  const oauth = {
+    callback: `${ssl + req.headers.host}/resultados?socialnetwork=twitter&authenticated=true&profile=${profile}&search_for=${search_for}&limit=${limit}#conteudo`,
     consumer_key: config.consumer_key,
     consumer_secret: config.consumer_secret,
   };
-  let url = 'https://api.twitter.com/oauth/request_token';
-  request.post({url: url, oauth: oauth}, function (err, r, body) {
+  const url = 'https://api.twitter.com/oauth/request_token';
+  request.post({ url, oauth }, (err, r, body) => {
     if (err) {
       callback(err, null);
     }
-    var req_data = qs.parse(body);
+    const req_data = qs.parse(body);
     if (!req_data.oauth_token || !req_data.oauth_token_secret) {
       callback(body, null);
     }
     mcache.put(req_data.oauth_token, req_data.oauth_token_secret, 3600 * 1000);
-    let uri = 'https://api.twitter.com/oauth/authenticate' + '?' + qs.stringify({oauth_token: req_data.oauth_token});
+    const uri = `${'https://api.twitter.com/oauth/authenticate' + '?'}${qs.stringify({ oauth_token: req_data.oauth_token })}`;
     callback(null, uri);
   });
 }
 
-function requestTwitterList(client, search_for, profile, limit, callback) {
-  let cursor = -1;
-  let list = new Array();
-  let total = 0;
-  let param = {
-    screen_name: profile,
-  };
-  client.get('users/show', param, function (error, tweets, response_twitter_user) {
-    if (error) {
-      console.log(error);
-    }
-    total = JSON.parse(response_twitter_user.body)[search_for + '_count'];
-    async.whilst(
-      function () { return cursor == -1; },
-      function (next) {
-        let params = {
-          screen_name: profile,
-          count: limit,
-          cursor: cursor,
-        };
-        client.get(search_for + '/list', params, function (error, tweets, response_twitter) {
-          if (error) {
-            console.log(error);
-            next(error);
-          } else {
-            let data = JSON.parse(response_twitter.body);
-            data.users.forEach(function (current) {
-              list.push(current);
-            });
-            cursor = data.next_cursor;
-            next();
-          }
-        });
-      },
-      function (err) {
-        let object = {
-          metadata: {
-            count: list.length,
-            total: total,
-          },
-          profiles: [],
-        };
-        list.forEach(function (value) {
-          object.profiles.push({
-            username: value.screen_name,
-            url: 'https://twitter.com/' + value.screen_name,
-            avatar: value.profile_image_url,
-            user_profile_language: value.lang,
-          });
-        });
-        if (err) {
-          object.metadata.error = err;
-        }
-        callback(object);
-      }
-    );
-  });
-}
 
-app.post('/feedback', function (request, response) {
-  let opinion = request.body.opinion;
-  let profile = request.body.profile;
+app.post('/feedback', (request, response) => {
+  const { opinion } = request.body;
+  const { profile } = request.body;
   if (!opinion || !profile) {
     response.status(400).send('JSON Parameters opinion and profile required.');
     return;
@@ -205,9 +208,9 @@ app.post('/feedback', function (request, response) {
     response.status(400).send('profile should be a JSON object and need to contains at least an username and a bot_probability.');
     return;
   }
-  let screen_name = profile.username;
+  const screen_name = profile.username;
   if (fs.existsSync('opinion.json') === false) {
-    let object = {
+    const object = {
       approve: {
         profiles: [],
       },
@@ -217,11 +220,9 @@ app.post('/feedback', function (request, response) {
     };
     fs.writeFileSync('opinion.json', JSON.stringify(object));
   }
-  let content = fs.readFileSync('opinion.json');
-  let data = JSON.parse(content);
-  let index = data[opinion].profiles.findIndex(function (element) {
-    return element.username === screen_name;
-  });
+  const content = fs.readFileSync('opinion.json');
+  const data = JSON.parse(content);
+  const index = data[opinion].profiles.findIndex((element) => element.username === screen_name);
 
   if (index !== -1) {
     if (data[opinion].profiles[index].bot_probability.all <= (profile.bot_probability.all + 0.10) && data[opinion].profiles[index].bot_probability.all >= (profile.bot_probability.all - 0.10)) {
@@ -239,12 +240,12 @@ app.post('/feedback', function (request, response) {
   response.send('OK');
 });
 
-app.get('/feedback', function (request, response) {
+app.get('/feedback', (request, response) => {
   if (fs.existsSync('opinion.json') === false) {
     response.send('No feedback yet');
     return;
   }
-  let content = fs.readFileSync('opinion.json');
-  let data = JSON.parse(content);
+  const content = fs.readFileSync('opinion.json');
+  const data = JSON.parse(content);
   response.json(data);
 });
