44d4915e0d08583c416fc4ca882e9d5a2d3998c8
Junior Moraes <juniorfvox@gmail.com>
2020-05-09 20:24:40 -0300

    refactor: use async await on cli tool.

--- src/cli.js
+++ src/cli.js
@@ -12,20 +12,20 @@ const config = {
 const screenName = process.argv[2];
 if (!screenName) {
   console.error('Please specify a screen name to analyze as an argument. Example: npm run cli <screen_name>');
-} else {
-  console.log(`Starting user ${screenName} analysis...`);
-
-  analyze(screenName, config, { friend: false, sentiment: false }).then((info) => {
-    const userScore = Math.round(info.profiles[0].language_independent.user * 100);
-    const friendsScore = Math.round(info.profiles[0].language_independent.friend * 100);
-    const temporalScore = Math.round(info.profiles[0].language_independent.temporal * 100);
-    const networkScore = Math.round(info.profiles[0].language_independent.network * 100);
-    const sentimentScore = Math.round(info.profiles[0].language_dependent.sentiment.value * 100);
-    const finalScore = Math.round(info.profiles[0].bot_probability.all * 100);
-
-    const msg = `User score: ${userScore}%\nFriends score: ${friendsScore}%\nTemporal score: ${temporalScore}%\nNetwork score ${networkScore}%\nSentiment score: ${sentimentScore}%\nFinal score: ${finalScore}%`;
-    console.log(msg);
-  }).catch((err) => {
-    console.error(err);
-  });
+  process.exit();
 }
+
+(async () => {
+  console.log(`Starting user ${screenName} analysis...`);
+  const info = await analyze(screenName, config, { friend: false, sentiment: false });
+
+  const userScore = Math.round(info.profiles[0].language_independent.user * 100);
+  const friendsScore = Math.round(info.profiles[0].language_independent.friend * 100);
+  const temporalScore = Math.round(info.profiles[0].language_independent.temporal * 100);
+  const networkScore = Math.round(info.profiles[0].language_independent.network * 100);
+  const sentimentScore = Math.round(info.profiles[0].language_dependent.sentiment.value * 100);
+  const finalScore = Math.round(info.profiles[0].bot_probability.all * 100);
+
+  const msg = `User score: ${userScore}%\nFriends score: ${friendsScore}%\nTemporal score: ${temporalScore}%\nNetwork score ${networkScore}%\nSentiment score: ${sentimentScore}%\nFinal score: ${finalScore}%`;
+  console.log(msg);
+})();
