f10036574e01e90d814140f347cd58fc079ba666
Jordan Victor Scher <jordanvscher@hotmail.com>
2020-05-19 19:18:42 -0300

    test: add /botometer - not authenticated

--- test/app.spec.js
+++ test/app.spec.js
@@ -1,8 +1,10 @@
-import supertest from 'supertest';
-import { describe, it } from 'mocha';
-
 import assert from 'assert';
+import { describe, it } from 'mocha';
+import supertest from 'supertest';
+import sinon from 'sinon';
+import TwitterLite from 'twitter-lite';
 import app from '../src/app';
+
 
 describe('GET /botometer - missing params', () => {
   const params = [
@@ -19,6 +21,29 @@ describe('GET /botometer - missing params', () => {
         .end((err, res) => {
           assert(res.status === 400);
           assert(res.text === 'One parameter is missing');
+          done();
+        });
+    });
+  });
+});
+
+describe('GET /botometer - followers/friends, not authenticated', () => {
+  const params = [
+    { search_for: 'followers', profile: 'foobar' },
+    { search_for: 'friends', profile: 'foobar' },
+  ];
+
+  const token = '123';
+  sinon.stub(TwitterLite.prototype, 'getRequestToken').returns({ oauth_token: token });
+
+  params.forEach((testParam) => {
+    it('status code 200 and url', (done) => {
+      supertest(app)
+        .get('/botometer')
+        .query(testParam)
+        .end((err, res) => {
+          assert(res.status === 200);
+          assert(res.body.request_url === `https://api.twitter.com/oauth/authenticate?oauth_token=${token}`);
           done();
         });
     });
