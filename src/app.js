a90bc26635810e63c15b1de5b0b07ea9d7b7a644
Lucas Ansei <lucas.ansei@appcivico.com>
2020-11-11 17:46:54 -0300

    Updating headers

--- src/app.js
+++ src/app.js
@@ -102,8 +102,8 @@ async function getTokenUrl(req, searchFor, profile, limit) {
 }
 
 app.all('/', function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("Access-Control-Allow-Origin", "*, always");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With, Origin, Accept");
   next();
  });
 
