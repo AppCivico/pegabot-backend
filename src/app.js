bdc53d810b7b950cc3bf8e29e50047f76c8a44fa
Lucas Ansei <lucas.ansei@appcivico.com>
2020-11-11 17:05:36 -0300

    Adding support for CORS

--- src/app.js
+++ src/app.js
@@ -101,6 +101,12 @@ async function getTokenUrl(req, searchFor, profile, limit) {
   }
 }
 
+app.all('/', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+ });
+
 app.get('/botometer', async (req, res) => {
   console.log(req.headers);
 
